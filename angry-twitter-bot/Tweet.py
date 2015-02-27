de03ff39937d13ed6b68e529a57ed057c3f747c7
Shakti <shakti@Shaktis-MacBook-Pro.local>
2015-02-27 08:11:25 -0500

    first commit

---
+++ angry-twitter-bot/Tweet.py
@@ -0,0 +1,25 @@
+from cred import settings
+import twitter
+import time
+
+consumer_key = settings['consumer_key']
+consumer_secret = settings['consumer_secret']
+access_token_key = settings['access_token_key']
+access_token_secret = settings['access_token_secret']
+
+tweet = 'Someone gonna get a hurt read-bad @pinterest'
+
+print tweet
+
+try:
+	api = twitter.Api(
+	consumer_key = consumer_key,
+	consumer_secret = consumer_secret,
+	access_token_key = access_token_key,
+	access_token_secret = access_token_secret)
+ 
+	status = api.PostUpdate(tweet)
+	print '  post successful!'
+ 
+except twitter.TwitterError:
+	print 'error posting!'
---
+++ angry-twitter-bot/cred.py
@@ -0,0 +1,9 @@
+import twitter
+
+settings = {
+'consumer_key': 'ZLczX00qoaXEN6FKsY9Uvbz2y',
+'consumer_secret': 'IP4HEy7lngxZwoH8B5WSpBrTTZ6iT0FbNHMVFJARoOSaF4jLAn',
+'access_token_key': '3064238577-Radj2xRcx4lF3tNYZWzDbAnKLZ3jJuYfls9Cq4j',
+'access_token_secret': 'AvvhhfffCknPPqXZwPyfezajoxlFo8XjPYTS2V7lO3Vwo'
+}
+
